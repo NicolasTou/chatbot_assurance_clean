f95e34d1a894119b6d3bc7d3ca140600e95fa3d0
Nicolas Toulemonde <nicolas.toulemonde@hotmail.fr>
2025-07-07 14:29:23 +0200

    Procfile & main

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = FastAPI()
 class Query(BaseModel):
     question: str
 
+@app.get("/")
+def helloworld()
+    return "Hello World"
 @app.post("/ask")
 def ask_bot(query: Query):
     return {"response": get_bot_response(query.question)}
