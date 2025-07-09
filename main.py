b9bde924e6b0d61517102d719e78ada667aef703
Nicolas Toulemonde <nicolas.toulemonde@hotmail.fr>
2025-07-09 12:55:42 +0200

    Ajout du démarrage Uvicorn pour Railway

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ def helloworld():
 def ask_bot(query: Query):
     return {"response": get_bot_response(query.question)}
 
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
